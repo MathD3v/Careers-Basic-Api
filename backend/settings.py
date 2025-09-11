4ee8222389d7456b606cfcd5f6c9947c093c5add
Matheus <78439058+MathD3v@users.noreply.github.com>
2025-09-11 17:26:48 -0300

    fix: ajuste paginação

--- backend/settings.py
+++ backend/settings.py
@@ -64,6 +64,8 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,  # número de itens por página usado para aumentar performance
 }
 
 ROOT_URLCONF = 'backend.urls'
